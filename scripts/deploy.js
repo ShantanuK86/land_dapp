7e6348633cbf758066bf03857eb7d7300c934520
ShantanuK86 <sk9411011994@gmail.com>
2024-03-02 17:09:12 +0530

    implemented deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,16 @@
-
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy the Land contract
+  const Land = await hre.ethers.getContractFactory("Land");
+  const land = await Land.deploy();
+  await land.deployed();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Print the deployed contract address
+  console.log("Land contract deployed to:", land.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+// Run the deployment script
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
